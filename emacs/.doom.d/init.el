ea6a78476286070d01aa1f77bc783a06f038ae40
Josh Seba <sebajosh@outlook.com>
2018-04-05 12:24:59 -0700

    Remove unused DOOM modules

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -8,9 +8,6 @@
        eval              ; run code, run (also, repls)
        (evil +everywhere); come to the dark side (we have cookies)
        file-templates    ; auto-snippets for empty files
-       (lookup           ; helps you navigate your code and documentation
-        +devdocs         ; ...on devdocs.io online
-        +docsets)        ; ...or in Dash docsets locally
       ;services          ; TODO managing external services & code builders
        snippets          ; my elves. They type so I don't have to
        spellcheck        ; tasing you for misspelling mispelling
@@ -26,10 +23,8 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
-       evil-goggles      ;
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
-       vi-tilde-fringe   ;
        window-select     ; visually switch windows
        posframe          ; use child frames where possible (Emacs 26+ only)
 
@@ -40,7 +35,6 @@
        imenu             ; an imenu sidebar and searchable code index
        make              ; run make tasks from Emacs
       ;magit             ; magit is magic
-       neotree           ; a project drawer, like NERDTree for vim
        pdf               ; pdf enhancements
        rotate-text       ; cycle region at point between text candidates
        term              ; terminals in Emacs
@@ -58,7 +52,6 @@
         +publish)        ; Emacs+Org as a static site generator
        perl              ; write code no one else can comprehend
        python            ; beautiful is better than ugly
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        sh                ; she sells (ba|z)sh shells on the C xor
 
        :app
