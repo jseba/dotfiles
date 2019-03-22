ab6e2699f199f5856424710a953c63003338aec4
Josh Seba <sebajosh@outlook.com>
2019-03-22 06:45:10 -0700

    Emacs: Improve Elisp config
    * Better Imenu support
    * Integrate with improved Xref
    * Compile on save

--- emacs/.emacs.d/lisp/config-elisp.el
+++ emacs/.emacs.d/lisp/config-elisp.el
@@ -17,6 +17,41 @@
 (+pretty-code-set-symbols 'emacs-lisp-mode
   :lambda "lambda")
 
+(add-hook! 'emacs-lisp-mode-hook
+  #'(outline-minor-mode
+     +elisp-extend-imenu
+     rainbow-delimiters-mode
+     highlight-quoted-mode))
+(setq-hook! 'emacs-lisp-mode-hook
+  mode-name "Elisp"
+  outline-regexp "[ \t]*;;;;* [^ \t\n]")
+(+xref-set-lookup-handlers 'emacs-lisp-mode
+  :definition #'elisp-def
+  :documentation #'+elisp-lookup-documentation)
+
+(defun +elisp-extend-imenu ()
+  "Improve Imenu support with better expression regexps."
+  (setq imenu-generic-expression
+        '(("Evil commands" "^\\s-*(evil-define-\\(?:command\\|operator\\|motion\\) +\\(\\_<[^ ()\n]+\\_>\\)" 1)
+          ("Major modes"   "^\\s-*(defined-derived-mode +\\([^ ()\n]+\\)" 1)
+          ("Modelines"     "^\\s-*(def-modeline! +\\([^ ()\n]+\\)" 1)
+          ("Modeline segments" "^\\s-*(def-modeline-segment! +\\([^ ()\n]+\\)" 1)
+          ("Advice"        "^\\s-*(def\\(?:\\(?:ine-\\)?advice\\))")
+          ("Modes"         "^\\s-*(define-\\(?:global\\(?:ized\\)?-minor\\|generic\\|minor\\)-mode +\\([^ ()\n]+\\)" 1)
+          ("Macros"        "^\\s-*(\\(?:cl-\\)?def\\(?:ine-compile-macro\\|macro\\) +\\([^ )\n]+\\)" 1)
+          ("Inline functions" "\\s-*(\\(?:cl-\\)?defsubst +\\([^ )\n]+\\)" 1)
+          ("Functions"     "^\\s-*(\\(?:cl-\\)?def\\(?:un\\|un\\*\\|method\\|generic\\|-memoized!\\) +\\([^ ,)\n]\\)" 1)
+          ("Variables"     "^\\s-*\\(def\\(?:c\\(?:const\\(?:ant\\)?\\|ustom\\)\\|ine-symbol-macro\\|parameter\\|var\\(?:-local\\)?\\)\\)\\s-+\\(\\(?:\\sw\\|\\s_\\|\\\\.\\)+\\)" 2)
+          ("Types"         "^\\s-*(\\(cl-def\\(?:struct\\|type\\)\\|def\\(?:class\\|face\\|group\\|ince-\\(?:condition\\|error\\|widget\\)\\|package\\|struct\\|t\\(?:\\(?:hem\\yp\\)e\\)\\)\\)\\s-+'?\\(\\(?:\\sw\\|\\s_\\|\\\\.\\)+\\)" 2))))
+
+(defun +elisp-lookup-documentation (thing)
+  "Lookup THING with `helfpul-variable' if it's a variable, `apropos' otherwise."
+  (cond ((not thing)
+         (call-interactively #'helpful-symbol))
+        ((if-let* ((sym (intern-soft thing))) (helpful-symbol sym)))
+        ((apropos (format "^%s" thing)))
+        ((apropos thing))))
+
 (defun +elisp-highlight-vars-and-faces (end)
   "Match defined variables and functions.
 
@@ -63,6 +98,9 @@ library/userland functions."
 
 (use-package highlight-quoted
   :hook (emacs-lisp-mode-hook . highlight-quoted-mode))
+(use-package auto-compile
+  :hook (emacs-lisp-mode-hook . auto-compile-on-save-mode))
+(use-package elisp-def)
 
 (provide 'config-elisp)
 ;;; config-elisp.el ends here
